73205caeb83e4b4445803c916fc1879343053c98
Andrew <klinks22@gmail.com>
2022-03-23 09:44:52 -0400

    Add database, models & automigration

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fiber_intro/database"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	// Start a new fiber app
 	app := fiber.New()
+
+	database.ConnectDB()
 
 	// Send a string back for GET calls to the endpoint "/"
 	app.Get("/", func(c *fiber.Ctx) error {
